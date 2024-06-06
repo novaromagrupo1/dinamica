20fdd1bc6fb5e6582d03eac3ac21e9386343ea0d
Driggsz <seuemail@example.com>
2024-06-06 17:35:10 -0300

    Fix UsersController e Edit Form edit

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -6,21 +6,19 @@ function UserController() {
   function list(req, res) {
     User.findAll({ raw: true })
       .then((data) => {
-
         res.render('users/list', { 
-          title: "Lista de Tarefas",
+          title: "Lista de Usuários", 
           users: data, 
-        })
+        });
       })
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   }
 
   function create(req, res) {
-    res.render('users/create')
+    res.render('users/create');
   }
 
   async function save(req, res) {
-    
     const body = req.body;
 
     if (!body.password) {
@@ -47,7 +45,7 @@ function UserController() {
           message: "Os campos senha e confirmar senha devem ser iguais!",
         },
       });
-      return
+      return;
     }
 
     const hashed_password = await bcrypt.hash(req.body.password, 10);
@@ -56,13 +54,13 @@ function UserController() {
       name: req.body.name,
       email: req.body.email,
       password: hashed_password,
-    }
+    };
 
     try {
       await User.create(user);
       res.redirect('/users');
     } catch (error) {
-      console.log(error);      
+      console.log(error);
     }
   }
 
@@ -70,56 +68,59 @@ function UserController() {
     const id = req.params.id;
 
     User.destroy({ where: { id: id } })
-      .then(res.redirect('/users'))
-      .catch((err) => console.log(err))
+      .then(() => res.redirect('/users'))
+      .catch((err) => console.log(err));
   }
 
   function edit(req, res) {
-    const id = req.params.id
+    const id = req.params.id;
 
     User.findOne({ where: { id: id }, raw: true })
       .then((data) => {
-        res.render('users/edit', { user: data })
+        res.render('users/edit', { user: data });
       })
-      .catch((err) => console.log())
+      .catch((err) => console.log(err));
   }
 
   function update(req, res) {
-    console.log(req.body);
-    const id = req.body.id
+    const id = req.body.id;
 
     const user = {
-      title: req.body.title,
-      description: req.body.description,
-      done: req.body.done === '1' ? true : false
-    }
+      name: req.body.name,
+      email: req.body.email,
+      active: req.body.active === '1',  // Tratando o campo active como booleano
+    };
 
     User.update(user, { where: { id: id } })
-      .then(res.redirect('/users'))
-      .catch((err) => console.log(err))
+      .then(() => res.redirect('/users'))
+      .catch((err) => console.log(err));
   }
 
   function updateStatus(req, res) {
-    const id = req.params.id
+    const id = req.params.id;
 
-    const user = {
-      done: req.body.done === '0' ? true : false,
-    }
+    User.findByPk(id)
+      .then(user => {
+        if (user) {
+          const newStatus = !user.active;
+          return User.update({ active: newStatus }, { where: { id: id } });
+        } else {
+          throw new Error('User not found');
+        }
+      })
+      .then(() => res.redirect('/users'))
+      .catch((err) => console.log(err));
+  }
 
- 	  User.update(user, { where: { id: id } })
-      .then(res.redirect('/users'))
-      .catch((err) => console.log())
-    }
-
-    return {
-      create,
-      save,
-      list,
-      remove,
-      edit,
-      update,
-      updateStatus,
-    }
+  return {
+    create,
+    save,
+    list,
+    remove,
+    edit,
+    update,
+    updateStatus,
+  };
 
 }
 
